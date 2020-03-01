e7c7d48cbe8768add0a8ee3522faf16f2153e462
block439 <51018613+block439@users.noreply.github.com>
2020-03-01 22:11:08 +0100

    Średnia

--- app/src/main/java/pl/jazapp/app/AverageServlet.java
+++ app/src/main/java/pl/jazapp/app/AverageServlet.java
@@ -15,9 +15,16 @@ public class AverageServlet extends HttpServlet {
         resp.setContentType("text.plain");
 
         var numbers = req.getParameter("numbers");
-
+        String[] numberSplit = numbers.split(",");
+        float sum = 0;
+        for (int i=0; i<numberSplit.length;i++){
+            sum += Integer.parseInt(numberSplit[i]);
+        }
+        float avg = sum/numberSplit.length;
+//liczby przeliczaja sie na floata trzeba to jeszcze wysplitowac w przypadku 0 na koncu liczby.
 
         var writer = resp.getWriter();
-        writer.println("I got numbers '"+numbers+"'");
+
+        writer.println("I got numbers '"+numbers+"'"+sum+" "+avg);
     }
 }
