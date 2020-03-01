8141673faf578553f7d2ae877d6adfe9a79413a2
block439 <51018613+block439@users.noreply.github.com>
2020-03-01 21:26:27 +0100

    zakonczneie zajec 01.03.2020

--- app/src/main/java/pl/jazapp/app/App.java
+++
@@ -1,7 +0,0 @@
-package pl.jazapp.app;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ app/src/main/java/pl/jazapp/app/AverageServlet.java
@@ -0,0 +1,23 @@
+package pl.jazapp.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("average")
+public class AverageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
+        resp.setContentType("text.plain");
+
+        var numbers = req.getParameter("numbers");
+
+
+        var writer = resp.getWriter();
+        writer.println("I got numbers '"+numbers+"'");
+    }
+}
