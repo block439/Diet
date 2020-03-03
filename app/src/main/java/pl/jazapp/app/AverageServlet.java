16cc247d3ff929da4dff48541c8e9b8109a65254
block439 <51018613+block439@users.noreply.github.com>
2020-03-03 22:09:13 +0100

    Średnia 03.03.2020

--- app/src/main/java/pl/jazapp/app/AverageServlet.java
+++ app/src/main/java/pl/jazapp/app/AverageServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 @WebServlet("average")
 public class AverageServlet extends HttpServlet {
@@ -15,23 +16,28 @@ public class AverageServlet extends HttpServlet {
         resp.setContentType("text.plain");
 
         var numbers = req.getParameter("numbers");
-        String[] splitedNumbers = numbers.split(",");
-        double sum = 0;
-        for (int i=0; i<splitedNumbers.length;i++){
-            sum += Integer.parseInt(splitedNumbers[i]);
-        }
-        double avg = sum/splitedNumbers.length;
-
         var writer = resp.getWriter();
 
-        //do rozwiązania opcja z nullem i skrócenie formatowania dla 1.3333 np
-        String roundedAverage = String.valueOf(avg);
-        if(".0".equals(roundedAverage.substring(roundedAverage.length()-2)))
-           writer.println("sub " + roundedAverage.substring(0,roundedAverage.length()-2));
-        else if(numbers == null)
-            writer.println("pusto");
+
+        if (numbers != null) {
+            String[] splitedNumbers = numbers.split(",");
+
+            double sum = 0;
+
+            for (int i = 0; i < splitedNumbers.length; i++) {
+                sum += Integer.parseInt(splitedNumbers[i]);
+            }
+
+            sum = (double) Math.round(sum / splitedNumbers.length * 100d) / 100d;
+
+            String roundedAverage = String.valueOf(sum);
+            if (roundedAverage.substring(roundedAverage.length() - 2).endsWith(".0"))
+                writer.println("Average equals: " + roundedAverage.substring(0, roundedAverage.length() - 2));
+            else
+                writer.println("Average equals: " + roundedAverage);
+        }
         else
-            writer.println("zwykla "+ roundedAverage);
+            writer.println("Please put parameters.");
 
     }
 }
